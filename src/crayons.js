73b1ac86f44fb228dcc8dc72a353e18a6b84f861
Josef Krajkář <josef.krajkar@mensa.cz>
2017-06-15 11:55:17 +0200

    Add files via upload

--- src/crayons.js
+++ src/crayons.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './crayons.css';
 
+//functional component for canvas
 function Platno(props) {
     return <canvas onMouseDown={props.onMouseDown}
         onMouseMove={props.onMouseMove}
@@ -11,28 +12,35 @@ function Platno(props) {
         style={props.style} />;
 }
 
-function Guma(props) {
+//functional component for the eraser cursor
+function Erasertool(props) {
     let stats = {
         top: props.y,
         left: props.x,
         visibility: props.visibility,
     };
-    return <div className='guma' draggable='false' style={stats} />;
+    return <div className='eraserTool' draggable='false' style={stats} />;
 }
 
+//functional component for the shadow behind a canvas
 function Stin() {
     return <div className="shadow" />;
 }
 
+//functional component for the "New" button
 function Newbtn(props) {
-    return <button className="newbtn" type="button" onClick={props.onClick}>New</button>
+    return <button className='newButton' type="button" onClick={props.onClick}><b>New</b></button>
 }
 
+//functional component for the Menu at the bottom of the screen
 function Menu(props) {
     return <div className="menu">{props.children}</div>;
 }
 
+//Main class responsible for the correct beheaviour of the Crayons component
 class Crayons extends Component {
+
+    //constructor method with "declaration" of the state property
     constructor() {
         super();
         this.state = {
@@ -53,6 +61,7 @@ class Crayons extends Component {
         }
     }
 
+    //when component mounts, the canvas and context properties are set to the state 
     componentDidMount() {
         let canvas = document.getElementById("canvas1");
         if (this.state.canvas != null) {
@@ -64,6 +73,7 @@ class Crayons extends Component {
         }
     }
 
+    //method for handling the mouse move over the canvas
     handleMove(e) {
         if (this.state.eraser) {
             this.setState({
@@ -89,6 +99,7 @@ class Crayons extends Component {
         }
     }
 
+    //method for handling the mouse down event on the canvas
     handleClick(e) {
         if (this.state.eraser) {
             this.setState({
@@ -111,18 +122,21 @@ class Crayons extends Component {
         }
     }
 
+    //method for changing the color of a line
     changeColor(i) {
         this.setState({
             color: this.chooseColor(i),
         });
     }
 
+    //method for changing the width of a line
     changeWidth(i) {
         this.setState({
             lineWidth: parseInt(i.target.value, 10),
         });
     }
 
+    //method called when the mouse button is released over the canvas
     endDraw() {
         if (this.state.erasing) {
             this.setState({
@@ -138,6 +152,7 @@ class Crayons extends Component {
         }
     }
 
+    //method called when the cursor leaves the canvas area
     outOfCanvas() {
         if (this.state.eraser) {
             this.setState({
@@ -154,16 +169,21 @@ class Crayons extends Component {
         }
     }
 
+    //method called when the "New" button is clicked - after confirmation will clear the canvas
     newPic() {
-        let context = this.state.context;
-        context.clearRect(0, 0, this.state.canvas.width, this.state.canvas.height);
+        if(confirm("Clear the canvas?")===true) {
+            let context = this.state.context;
+            context.clearRect(0, 0, this.state.canvas.width, this.state.canvas.height);
+        }
     }
 
+    //method used by component for choosing proper color from the array of colors
     chooseColor(i) {
         const colors = ['black', 'white', 'grey', 'blue', 'green', 'red', 'brown', 'yellow', 'orange', 'purple'];
         return colors[i];
     }
 
+    //method for rendering the "color" button
     renderClrBtn(i) {
         let btnStyle = {
             background: this.chooseColor(i),
@@ -171,6 +191,7 @@ class Crayons extends Component {
         return <button type='button' className='clrbtn' onClick={() => this.changeColor(i)} style={btnStyle} />
     }
 
+    //method rendering square with the chosen color
     renderInnerSquare() {
         let squareStyle = {
             background: this.state.color,
@@ -178,6 +199,7 @@ class Crayons extends Component {
         return <div style={squareStyle} className="innerSquare" />;
     }
 
+    //method called when the "Eraser" button is clicked - handles turning the eraser on and off
     handleEraser() {
         this.setState({
             eraser: !this.state.eraser,
@@ -188,6 +210,7 @@ class Crayons extends Component {
 
     }
 
+    //method rendering the eraser part of the menu 
     renderEraserMenu() {
         let eraserStyle = {};
         if (this.state.eraser) {
@@ -204,7 +227,7 @@ class Crayons extends Component {
         }
         return (<div className='eraser'>
             <p className='eraserText'>Eraser:</p>
-            <div className='eraserTool'>
+            <div className='eraserButton'>
                 <div className='eraserInnerSquare' style={eraserStyle} onClick={() => this.handleEraser()}>
                     <p className='eraserE'>E</p>
                 </div>
@@ -212,6 +235,7 @@ class Crayons extends Component {
         </div>);
     }
 
+    //main method rendering the Crayons component
     render() {
         return <div className="area"><Stin /><Platno
             id="canvas1" style={this.state.cursorStyle}
@@ -220,9 +244,14 @@ class Crayons extends Component {
             onMouseOut={() => this.outOfCanvas()}
             onMouseUp={() => this.endDraw()}>
         </Platno>
-            <Guma x={this.state.eraserX} y={this.state.eraserY} visibility={this.state.eraserVisibility} />
+            <Erasertool x={this.state.eraserX} y={this.state.eraserY} visibility={this.state.eraserVisibility} />
             <Menu>
-                <Newbtn onClick={() => this.newPic()} />
+                <div className='newMenu'>
+                    <p className='newText'>
+                        New canvas:
+                    </p>
+                    <Newbtn onClick={() => this.newPic()} />
+                </div>
                 {this.renderEraserMenu()}
                 <div className="style">
                     <p className='styleText'>Line width:</p>
